b1f9f239ac318ca66cf2ccd73fe93756edd668ad
benleolee <benleolee@163.com>
2017-08-25 00:12:29 +0800

    Test

---
+++ HFJ01/Party.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+	public void buildnvite(){
+		Frame f = new Frame();
+		Label l = new Label("Party at Tim's");
+		Button b = new button("You bet");
+		Button c = new button("Shoot me");
+		Panel p = new Panel();
+		p.add(l);
+	}
+}
